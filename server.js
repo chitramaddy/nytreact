292ee1054b17ea84f37dce39bff05d7057134d7f
Your Name <chitramaddy@gmail.com>
2018-06-25 18:55:32 -0400

    client folder

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -14,13 +16,15 @@ if (process.env.NODE_ENV === "production") {
 }
 
 //API Routes
+app.use(routes);
 
 //Send other requests to React
 //This should be the last route described
-
 app.get("*", (req, res) => {
     res.sendFile(path.join(_dirname, "./client/build/index.html"));
 });
+
+mongoose.connect((process.env.MONGODB_URI) || "mongodb://localhost/nytreact");
 
 app.listen(PORT, () => {
     console.log (`🌎 ==> Server now on port ${PORT}!`);
