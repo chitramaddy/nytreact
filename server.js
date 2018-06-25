bfec62268342bd7847a524967caf516a2b3ff263
Your Name <chitramaddy@gmail.com>
2018-06-25 13:34:41 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//define middelware
+
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+//API Routes
+
+//Send other requests to React
+//This should be the last route described
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(_dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log (`🌎 ==> Server now on port ${PORT}!`);
+});
